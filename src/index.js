992731dd2c42b44bf62ea78bfd9585ff83f00bc4
Ivan <ivanfrasca92@gmail.com>
2022-08-23 17:46:53 +0200

    jsx-02-done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from "react";
+import  ReactDOM  from "react-dom";
 
-import  ReactDOM  from "react-dom/client";
+export class App extends React.Component {
+  render() {
+    return (
+      <div>
+        {helloSomeone("Ciccio")}
+      </div>
+    );
+  }
+}
+
+function helloSomeone(name) {
+  return <h1>Hello, {name}</h1>;
+}
 
 const container = document.querySelector("#root")
 
 const root = ReactDOM.createRoot(container)
 
-const hello = <h1> Hello, World!!!</h1>
-
-root.render(hello)
+root.render(<App />)
