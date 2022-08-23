78fcf4dc40207d56d745cc1d32e7892010b9586f
Ivan <ivanfrasca92@gmail.com>
2022-08-23 17:49:29 +0200

    jsx-02-done-for-real

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
 import React from "react";
 import  ReactDOM  from "react-dom";
 
-export class App extends React.Component {
-  render() {
-    return (
-      <div>
-        {helloSomeone("Ciccio")}
-      </div>
-    );
-  }
-}
-
 function helloSomeone(name) {
   return <h1>Hello, {name}</h1>;
 }
@@ -19,4 +9,4 @@ const container = document.querySelector("#root")
 
 const root = ReactDOM.createRoot(container)
 
-root.render(<App />)
+root.render(helloSomeone("Ciccio"))
